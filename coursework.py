4b6e90c68cfd56132819afec97a41e16fd0a3772
AUDI Timothy (MCG) <Timothy.Audi@ke.multichoice.com>
2026-07-05 18:49:45 +0300

    added coursework.py

---
+++ coursework.py
@@ -0,0 +1,7 @@
+"""Data structures and algorithms coursework."""
+data_list = [1, 2, 3, 4, 5] #List - append, pop, remove, insert, sort, rever 
+data_list.append(6)
+print(data_list)
+
+data_list.__add__([7, 8, 9])
+print(data_list)
